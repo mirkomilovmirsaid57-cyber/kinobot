b13aee0b43d69033b8c202faa17b3da321943201
mirkomilovmirsaid57-cyber <mirkomilovmirsaid57@gmail.com>
2026-05-17 23:57:44 +0500

    Update config.py

--- config.py
+++ config.py
@@ -4,7 +4,7 @@
 BOT_TOKEN = "8717154652:AAHljagqYLi8wdi5nMCypwN-_RetW5DL0m8"
 
 # PostgreSQL ulanish URL'i (Render.com tekin baza - o'rnatgandan keyin almashtiring)
-DATABASE_URL = "postgresql://kinobot_user:PAROL@dpg-XXXXX.oregon-postgres.render.com/kinobot"
+DATABASE_URL = "postgresql://kinobot_db_fkxa_user:aA862oDVNgBytWPTk4iwRi5vBv37JMqi@dpg-d850ncuq1p3s73eatdi0-a.oregon-postgres.render.com/kinobot_db_fkxa"
 
 # Admin Telegram ID'lari
 ADMIN_IDS = [
